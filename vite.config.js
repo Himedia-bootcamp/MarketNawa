735492ff04186ae90fda99d3edce42b48fce4242
악덕 <rudejrqkr97@gmail.com>
2024-07-03 12:54:06 +0900

    build: 서버 및 빌드 설정에 대한 vite.config.js 업데이트
    - vite.config.js 파일을 서버 및 빌드 설정을 위해 업데이트합니다.
    - 서버 설정과 빌드 설정을 변경했습니다.
    - 서버 포트를 5173으로 설정했습니다.
    - 빌드 결과물 디렉토리를 'dist'로 설정했습니다.
    - 환경 변수 VITE_APP_BACKEND_URL을 정의했습니다.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 5173,
+    },
+    build: {
+      outDir: 'dist',
+    },
+    define: {
+      'process.env': {
+        VITE_APP_BACKEND_URL: env.VITE_APP_BACKEND_URL
+      }
     }
   }
-})
+});
