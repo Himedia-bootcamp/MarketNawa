87f8e3573815aef7ba53304355f22ba5c8f63d62
chanyong <jcy168942@gmail.com>
2024-07-03 17:48:59 +0900

    feat: elasticsearch api client안쓰고 기본 URI로 구현

--- src/main/kotlin/marketnawa/be/ott/marketnawabe/controller/SearchController.kt
+++ src/main/kotlin/marketnawa/be/ott/marketnawabe/controller/SearchController.kt
@@ -1,11 +1,7 @@
 package marketnawa.be.ott.marketnawabe.controller
 
-import marketnawa.be.ott.marketnawabe.common.Criteria
-import marketnawa.be.ott.marketnawabe.common.PagingResponseDTO
-import marketnawa.be.ott.marketnawabe.common.ResponseDTO
 import marketnawa.be.ott.marketnawabe.document.SortingOrder
 import marketnawa.be.ott.marketnawabe.service.SearchService
-import marketnawa.be.ott.marketnawabe.util.IndexingUtil
 import org.apache.http.HttpHost
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
@@ -22,16 +18,30 @@ class SearchController (
     private val searchService: SearchService,
 ){
 
+//    @GetMapping("/search")
+//    fun searchByFoodName(
+//        @RequestParam keyword: String,
+//        @RequestParam lastCategory: String,
+//        @RequestParam order: String,
+//        @RequestParam from: Int = 0,
+//        @RequestParam size: Int = 10,
+//    ) : ResponseEntity<ResponseDTO>{
+//        val sortingOrder = SortingOrder.fromString(order)
+//        val brandResults: Any = searchService.searchByFoodName(keyword, lastCategory, sortingOrder, from, size)
+//        return ResponseEntity.ok().body(ResponseDTO(HttpStatus.OK, "조회", brandResults))
+//    }
+
     @GetMapping("/search")
     fun searchByFoodName(
         @RequestParam keyword: String,
-        @RequestParam lastCategory: String,
+        @RequestParam detailCategory: String,
         @RequestParam order: String,
         @RequestParam from: Int = 0,
         @RequestParam size: Int = 10,
-    ) : ResponseEntity<ResponseDTO>{
-        val sortingOrder = SortingOrder.fromString(order)
-        val brandResults: Map<String, PagingResponseDTO> = searchService.searchByFoodName(keyword, lastCategory, sortingOrder, from, size)
-        return ResponseEntity.ok().body(ResponseDTO(HttpStatus.OK, "조회", brandResults))
+    ) : ResponseEntity<Any> {
+//        val sortingOrder = SortingOrder.fromString(order)
+        val indexName = "market_food"
+        val brandResults: Any = searchService.searchByFoodName(indexName, keyword, detailCategory, order, from, size)
+        return ResponseEntity.ok().body(brandResults)
     }
 }
