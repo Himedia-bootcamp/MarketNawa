4c57dd3f91a73037974d6a961f2b06d20aa98c06
악덕 <rudejrqkr97@gmail.com>
2024-07-04 15:34:08 +0900

    fix: CORS 설정 수정
    - 새로운 CORS 설정을 추가하여 다중 출처 요청을 허용함.
    - 이를 통해 다양한 출처에서의 요청을 안전하게 처리할 수 있음.

--- src/main/kotlin/marketnawa/be/ott/marketnawabe/config/CorsConfig.kt
+++ src/main/kotlin/marketnawa/be/ott/marketnawabe/config/CorsConfig.kt
@@ -1,4 +1,4 @@
-package com.akatsuki.pioms.config
+package marketnawa.be.ott.marketnawabe.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
